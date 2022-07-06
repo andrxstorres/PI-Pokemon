8dbb59b1e723e8b75ed43824507ac32aa4bba519
Andres Torres <andrestorres9700@gmail.com>
2022-07-05 21:54:00 -0300

    Back-end running

--- PI-Pokemon-main/api/src/routes/pokemons.js
+++ PI-Pokemon-main/api/src/routes/pokemons.js
@@ -4,57 +4,116 @@ const axios = require("axios");
 const { Pokemon, Type } = require("../db.js");
 
 router.get("/", async (req, res) => {
+  //SI RECIBE NOMBRE POR QUERY
   const pokeName = req.query.name;
 
   try {
+    //VERIFICACION DEL NOMBRE POR QUERY
     if (pokeName) {
       const dbPokemon = await Pokemon.findOne({ where: { name: pokeName } });
+      //VERIFICACION SI ES QUE ES UN POKEMON EN LA DB
       if (dbPokemon) {
-        return res.send(dbPokemon);
+        //SE GENERA El ARRAY DE TIPOS A PARTIR DE LA TABLA INTERMEDIA pokemon_types
+        const dbPokemonTypes = await dbPokemon.getTypes();
+        const types = [];
+        dbPokemonTypes.forEach((e) => {
+          types.push(e.name);
+        });
+
+        return res.send({ ...dbPokemon.dataValues, types });
       } else {
-        const apiPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
-        return res.send(apiPokemon.data);
+        //SI NO EXISTE EN DB, PROCEDEMOS A BUSCARLO EN LA API
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+        //EXTRAEMOS TODOS LOS DATOS
+        const { height, id, name, weight } = data;
+        const image = data.sprites.other["official-artwork"].front_default;
+
+        const types = [];
+        data.types.map((e) => {
+          types.push(e.type.name);
+        });
+        //GENERAMOS UN OBJETO STATS CON K:V = STAT: VALOR
+        const stats = {};
+        data.stats.forEach((e) => {
+          if (e.stat.name !== "special-attack" && e.stat.name !== "special-defense") {
+            stats[e.stat.name] = e.base_stat;
+          }
+        });
+
+        const apiPokemonDetails = { id, name, image, types, stats, height, weight };
+        return res.send(apiPokemonDetails);
       }
-      // dbPokemon
-      //   ? res.send(dbPokemon)
-      //   : res.send(findAPIPokemonByName(pokeName))
+      //SI NO RECIBIMOS NOMBRE POR QUERY, DEVOLVEMOS TODOS LOS POKEMONS
     } else {
       const apiPokemons = await axios.get("https://pokeapi.co/api/v2/pokemon");
       const { results } = apiPokemons.data;
+      //DATA DE AXIOS GUARDA TODOS LOS POKEMONS EN OBJETOS DENTRO DE RESULTS
+      //CADA OBJETO TIENE NOMBRE DEL POKEMON Y URL HACIA SUS DETALLES
 
       const pokemonsToHome = [];
-
+      //CREAMOS UN ARRAY PARA HACER UNA SUB-QUERY POR CADA POKEMON DENTRO DE UN FOR OF
       for (let e of results) {
+        //A DIFERENCIA DE LAS VECES ANTERIORES, TOMAMOS EL NAME DEL OBJETO DE DATA.RESULTS
         const { name, url } = e;
         const { data } = await axios.get(url);
-        const unfilteredTypes = data.types;
+        const { height, id, weight } = data;
 
-        const filteredTypes = [];
-        unfilteredTypes.map((e) => {
-          filteredTypes.push(e.type.name);
+        const types = [];
+        data.types.map((e) => {
+          types.push(e.type.name);
         });
 
+        const stats = {};
+        data.stats.forEach((e) => {
+          if (e.stat.name !== "special-attack" && e.stat.name !== "special-defense") {
+            stats[e.stat.name] = e.base_stat;
+          }
+        });
+        // const stats = [];
+        // data.stats.forEach((e) => {
+        //   if (e.stat.name !== "special-attack" && e.stat.name !== "special-defense") {
+        //     stats.push({ [e.stat.name]: e.base_stat });
+        //   }
+        // });
+
         const image = data.sprites.other["official-artwork"].front_default;
-        let pokeToHome = { name, types: filteredTypes, image };
+        let pokeToHome = { id, name, image, types, stats, height, weight };
         pokemonsToHome.push(pokeToHome);
       }
 
       res.send(pokemonsToHome);
     }
   } catch (err) {
-    res.status(400).send({ message: `El Pokémon '${req.query.name}' no existe!`, err });
+    res.status(400).send({ message: `Ocurrió un error en la petición de la ruta /pokemons! Es posible que '${req.query.name}' no sea un Pokémon existente!`, err });
   }
 });
 
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const isUUID = !Number(id);
+  const paramsId = req.params.id;
+  const isUUID = !Number(paramsId);
   if (isUUID) {
-    const dbPokemon = await Pokemon.findByPK(id);
+    const dbPokemon = await Pokemon.findByPk(paramsId);
     res.send(dbPokemon);
   } else {
-    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    res.send(data);
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${paramsId}/`);
+    const { height, id, name, weight } = data;
+
+    const image = data.sprites.other["official-artwork"].front_default;
+
+    const types = [];
+    data.types.forEach((e) => {
+      types.push(e.type.name);
+    });
+
+    const stats = {};
+    data.stats.forEach((e) => {
+      if (e.stat.name !== "special-attack" && e.stat.name !== "special-defense") {
+        stats[e.stat.name] = e.base_stat;
+      }
+    });
+
+    const pokeDetailById = { id, name, image, types, stats, height, weight };
+    res.send(pokeDetailById);
   }
 });
 
@@ -73,24 +132,10 @@ router.post("/", async (req, res) => {
     });
 
     if (types) {
-      //SIN VERIFICAR TIPOS
       types.forEach(async (e) => {
         const typeToAdd = await Type.findOne({ where: { name: e } });
         await newPokemon.addType(typeToAdd);
       });
-      // PETICION
-      // const { data } = await axios.get("http://localhost:3001/types");
-      // const typesToSet = data.filter((e) => types.includes(e.name));
-      // console.log(typesToSet);
-      //TOMANDO LOS TIPOS DE LA DB
-      // const AllTypesResponse = await Type.findAll();
-      // const allTypes = [];
-      // AllTypesResponse.forEach((e) => {
-      //   const { id, name } = e.dataValues;
-      //   allTypes.push({ id, name });
-      // });
-      // const typesToSet = data.filter((e) => allTypes.includes(e.name));
-      // console.log(typesToSet);
     }
 
     res.status(201).send({ m: `Your new Pokémon '${name}' has been saved!` });
--- PI-Pokemon-main/api/src/utils.js
+++ PI-Pokemon-main/api/src/utils.js
@@ -1,12 +1,54 @@
 const { default: axios } = require("axios");
 
-// let id = 1154;
-
 module.exports = {
-  findAPIPokemonByName: async (name) => {
-    return ({ data } = await axios.get(`http://localhost:3001/pokemons?name=${name}`));
+  getAPIPokemonByName: async (pokeName) => {
+    try {
+      const { data } = await axios.get(`http://localhost:3001/pokemons?name=${pokeName}`);
+
+      const { height, id, name, weight } = data;
+
+      const image = data.sprites.other["official-artwork"].front_default;
+
+      const types = [];
+      data.types.forEach((e) => {
+        types.push(e.type.name);
+      });
+
+      const stats = {};
+      data.stats.forEach((e) => {
+        // let newStat = {e.name: }
+        if (e.stat.name !== "special-attack" || e.stat.name !== "special-defense") stats = { ...stats, [e.stat.name]: e.base_stat };
+      });
+
+      return { image, name, types, stats, height, weight, id };
+    } catch (err) {
+      return { m: `Something went wrong while searching for '${pokeName}'` };
+    }
   },
   // generatePokeId: async () => {
   //   return (id = id + 1);
+  // },
+  // getPokemonById: async (id) => {
+  //   const isId = Number(id);
+  //   if (id) {
+  //     const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  //     const { height, name, weight } = data;
+  //     console.log(data);
+  //     res.send(data);
+  //   } else {
+  //   }
+
+  //   pokemonToSend = {
+  //     image,
+  //     name,
+  //     types,
+  //     hp,
+  //     attack,
+  //     defense,
+  //     speed,
+  //     height,
+  //     weight,
+  //     id,
+  //   };
   // },
 };
